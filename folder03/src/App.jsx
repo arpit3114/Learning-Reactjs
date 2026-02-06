f4f691769251e637bf3fa5b19fa83d2dc4a343aa
arpit3114 <arpitgupta3114@gmail.com>
2026-02-06 17:43:48 +0530

    create folder03 basic counter project

---
+++ folder03/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // let counter = 5
+  let [counter, setCounter] = useState(15)
+
+  const addValue = ()=>{
+    // counter = counter +1
+    setCounter(counter + 1)
+  }
+
+  const removeValue = ()=>{
+    setCounter(counter-1)
+  }
+
+  return (
+    <>
+      <h1>React Working</h1>
+      <h2>count Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
